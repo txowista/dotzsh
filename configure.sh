38140ab263b29aa36b8955801aea31985ea72805
Alberto Caro <alberto.caro.m@gmail.com>
2017-04-01 09:20:38 +0200

    configure.sh dont'ask for fzf install. configure.zsh starts automatically zsh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,11 @@
+#!/usr/bin/sh 
+
 git clone https://github.com/albertoCaroM/dotzsh.git  ~/.dotzsh 
 cd ~/.dotzsh 
 git submodule update --init --recursive 
 ln -s ~/.dotzsh/fzf ~/.fzf  
-~/.fzf/install
+~/.fzf/install --all
 cp ~/.dotzsh/machinerc ~/.machinerc
 mv ~/.zshrc ~/.zshrc.old 
 ln -s ~/.dotzsh/zshrc ~/.zshrc 
+zsh
